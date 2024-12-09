cf9f91cc9126cacc50cc569059c4636541edc547
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-12-09 20:51:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
 import session from "express-session";
-import RedisStore from "connect-redis";
+import * as RedisStore from "connect-redis";
 import { createClient } from "redis";
 
 import { dirname } from "path";
