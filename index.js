c931a29c2124a84e4150951018b10a151137cb94
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-12-09 16:14:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
+import session from "express-session";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -14,6 +15,13 @@ app.use(bodyParser.urlencoded( { extended: true }));
 // Static Middleware
 app.use(express.static("public"));
 
+app.use(
+    session({
+        secret: "hard1coded1bad1key1for1example123",
+        resave: false,
+        saveUninitialized: true,
+    })
+)
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
@@ -31,11 +39,27 @@ app.post("/home", (req, res) => {
         type = "the weekend";
         adv = "it's time to have fun.";
     }
-    res.render("home.ejs", 
-        { 
-            name: req.body["name"],
-            dayType: type,
-            advice: adv,
+    req.session.userData = {
+        name: req.body["name"],
+        dayType: type,
+        advice: adv,
+    }
+    res.render("home.ejs", req.session.userData);
+});
+
+app.get("/home", (req, res) => {
+    if(req.session.userData) {
+        res.render("home.ejs", req.session.userData);
+    } else {
+        res.render("/"); // Redirect to the form if no session data exists
+    }
+});
+
+app.get("/ejs", (req, res) => {
+    res.render("ejs.ejs", 
+        {
+            name: "John Doe",
+            html: "<b>Hello World</b>",
         }
     );
 });
