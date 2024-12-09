829be2d8ddce88157daf7bf066adbd1c858144d6
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-12-09 18:09:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ import express from "express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
 import session from "express-session";
+import RedisStore from "connect-redis";
+import { createClient } from "redis";
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+
+// Create a Redis client
+const redisClient = createClient({
+  url: process.env.REDIS_URL,
+});
+
+redisClient.connect().catch(console.error);
 
 // body parser middleware
 app.use(bodyParser.urlencoded( { extended: true }));
@@ -17,11 +27,16 @@ app.use(express.static("public"));
 
 app.use(
     session({
+        store: new RedisStore({ client: redisClient }),
         secret: "hard1coded1bad1key1for1example123",
         resave: false,
         saveUninitialized: true,
+        cookie: {
+            secure: process.env.NODE_ENV === "production",
+            maxAge: 1000 * 60 * 60 * 24, //1 day
+        },
     })
-)
+);
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
