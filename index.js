452d4d933c1c218c6f172fdaf2ed5eb843c57504
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-12-09 21:16:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const port = 3000;
 
 // Create a Redis client
 const redisClient = createClient({
-  url: process.env.REDIS_URL,
+  url: process.env.REDIS_URL  || "redis://localhost:6379",
 });
 
 redisClient.connect().catch(console.error);
